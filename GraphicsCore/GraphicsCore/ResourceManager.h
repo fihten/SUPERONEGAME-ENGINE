77d50d7d2fc2996adca255510b612d917acc455e
Andrey Maly <fihten1991@gmail.com>
2022-07-11 10:13:05 +0300

    ResourceManager

---
+++ GraphicsCore/GraphicsCore/ResourceManager.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <map>
+#include <string>
+
+class ResourceManager
+{
+	class TechniqueResource
+	{
+
+	};
+	std::map<std::string, TechniqueResource> techniques;
+
+public:
+
+};
