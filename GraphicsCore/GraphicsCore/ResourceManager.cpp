ecb1c6bc320149a71d766682c37c62aadd38e823
Andrey Maly <fihten1991@gmail.com>
2022-07-13 11:38:53 +0300

    ResourceManager::registerTechnique

--- GraphicsCore/GraphicsCore/ResourceManager.cpp
+++ GraphicsCore/GraphicsCore/ResourceManager.cpp
@@ -1,2 +1,12 @@
 #include "ResourceManager.h"
 
+ResourceManager::RegisterMessage ResourceManager::registerTechnique(const std::string& techniqueName, const ID3DX11EffectTechnique* technique)
+{
+	if (techniques.count(techniqueName) != 0)
+		return RegisterMessage::TECHNIQUE_ALREADY_EXIST;
+
+	TechniqueResource& techniqueRes = techniques[techniqueName];
+	techniqueRes.technique = technique;
+
+	return RegisterMessage::OK;
+}
--- GraphicsCore/GraphicsCore/ResourceManager.h
+++ GraphicsCore/GraphicsCore/ResourceManager.h
@@ -2,15 +2,22 @@
 
 #include <map>
 #include <string>
+#include <d3dx11effect.h>
 
 class ResourceManager
 {
-	class TechniqueResource
+	struct TechniqueResource
 	{
-
+		const ID3DX11EffectTechnique* technique = nullptr;
 	};
 	std::map<std::string, TechniqueResource> techniques;
 
 public:
+	enum RegisterMessage
+	{
+		OK,
+		TECHNIQUE_ALREADY_EXIST
+	};
 
+	RegisterMessage registerTechnique(const std::string& techniqueName, const ID3DX11EffectTechnique* technique);
 };
