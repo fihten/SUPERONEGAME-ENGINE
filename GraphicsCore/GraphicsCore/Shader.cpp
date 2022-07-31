9a1cbf29437b5d96259dbe459b0763a6601360ee
Andrey Maly <fihten1991@gmail.com>
2022-07-31 09:24:58 +0300

    processShader()

--- GraphicsCore/GraphicsCore/Shader.cpp
+++ GraphicsCore/GraphicsCore/Shader.cpp
@@ -1,7 +1,7 @@
 #include "Shader.h"
 #include <d3dx11async.h>
 
-void Shader::init(ID3D11Device* device, LPCTSTR path)
+void processShader(ID3D11Device* device, LPCTSTR path)
 {
 	DWORD shaderFlags = 0;
 #if defined(DEBUG) || defined(_DEBUG)
@@ -9,6 +9,7 @@ void Shader::init(ID3D11Device* device, LPCTSTR path)
 	shaderFlags |= D3D10_SHADER_SKIP_OPTIMIZATION;
 #endif
 
+	ID3DX11Effect* mShader = nullptr;
 	ID3D10Blob* compiledShader = 0;
 	ID3D10Blob* compilationMsgs = 0;
 	D3DX11CompileFromFile(path, 0, 0, 0, "fx_5_0", shaderFlags, 0, 0, &compiledShader, &compilationMsgs, 0);
--- GraphicsCore/GraphicsCore/Shader.h
+++ GraphicsCore/GraphicsCore/Shader.h
@@ -2,10 +2,4 @@
 #include <d3dx11effect.h>
 #include <d3d11.h>
 
-class Shader
-{
-public:
-	void init(ID3D11Device* device, LPCTSTR path);
-private:
-	ID3DX11Effect* mShader = nullptr;
-};
+void processShader(ID3D11Device* device, LPCTSTR path);
