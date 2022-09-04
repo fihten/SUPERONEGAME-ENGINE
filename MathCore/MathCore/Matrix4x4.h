73657c6432093b606fad58d84f18c1f13e89bbc6
Andrey Maly <fihten1991@gmail.com>
2022-09-04 10:51:29 +0300

    Matrix4x4

---
+++ MathCore/MathCore/Matrix4x4.h
@@ -0,0 +1,10 @@
+#pragma once
+
+template<class value_type>
+class Matrix4x4
+{
+	value_type m[16];
+
+public:
+
+};
--- MathCore/MathCore/Vec4d.h
+++ MathCore/MathCore/Vec4d.h
@@ -11,6 +11,8 @@ public:
 
 	Vec4d<value_type> operator+(const Vec4d<value_type>& v) const;
 	Vec4d<value_type> operator-(const Vec4d<value_type>& v) const;
+
+	friend value_type dot(const Vec4d<value_type>& v1, const Vec4d<value_type>& v2);
 };
 
 template<class value_type>
@@ -23,4 +25,10 @@ template<class value_type>
 Vec4d<value_type> Vec4d<value_type>::operator-(const Vec4d<value_type>& v) const
 {
 	return Vec4d<value_type>(this->v[0] - v.v[0], this->v[1] - v.v[1], this->v[2] - v.v[2], this->v[3] - v.v[3]);
+}
+
+template<class value_type>
+value_type dot(const Vec4d<value_type>& v1, const Vec4d<value_type>& v2)
+{
+	return v1.v[0] * v2.v[0] + v1.v[1] * v2.v[1] + v1.v[2] * v2.v[2] + v1.v[3] * v2.v[3];
 }
