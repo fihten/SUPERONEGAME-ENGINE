da68ff15733a9b322989727742bddf9d242e1bc9
Andrey Maly <fihten1991@gmail.com>
2022-09-14 22:28:49 +0300

    create vertex and index buffers

--- GraphicsCore/GraphicsCore/GraphicsCore.cpp
+++ GraphicsCore/GraphicsCore/GraphicsCore.cpp
@@ -195,6 +195,38 @@ void GraphicsCore::draw(Mesh& mesh)
 			data += si.size;
 		}
 	}
+
+	// Create vertex buffer
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = bytes;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	vbd.StructureByteStride = 0;
+	
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = data;
+
+	ID3D11Buffer* mVB;
+	device->CreateBuffer(&vbd, &vinitData, &mVB);
+
+	// Create index buffer
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = mesh.getIndicies()->size() * sizeof(uint32_t);
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	ibd.StructureByteStride = 0;
+
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = mesh.getIndicies()->data();
+
+	ID3D11Buffer* mIB;
+	device->CreateBuffer(&ibd, &iinitData, &mIB);
+
+
 }
 
 bool GraphicsCore::initWindow(HINSTANCE instanceHandle, int show, WNDPROC WndProc)
