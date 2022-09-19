9dc95bb1c977162570cdfb61f34ed6f65ea71b17
Andrey Maly <fihten1991@gmail.com>
2022-09-19 22:29:08 +0300

    improve class Vec3d

--- MathCore/MathCore/Vec3d.h
+++ MathCore/MathCore/Vec3d.h
@@ -11,6 +11,11 @@ public:
 
 	Vec3d<value_type> operator+(const Vec3d<value_type>& v) const;
 	Vec3d<value_type> operator-(const Vec3d<value_type>& v) const;
+	Vec3d<value_type> operator*(const value_type& x) const;
+	Vec3d<value_type> operator/(const value_type& x) const;
+
+	value_type length();
+	value_type normalize();
 
 	friend value_type dot(const Vec3d<value_type>& v1, const Vec3d<value_type>& v2);
 	friend Vec3d<value_type> cross(const Vec3d<value_type>& v1, const Vec3d<value_type>& v2);
@@ -29,6 +34,18 @@ Vec3d<value_type> Vec3d<value_type>::operator-(const Vec3d<value_type>& v) const
 }
 
 template<class value_type>
+Vec3d<value_type>  Vec3d<value_type>::operator*(const value_type& x) const
+{
+	return Vec3d<value_type>(x * this->v[0], x * this->v[1], x * this->v[2]);
+}
+
+template<class value_type>
+Vec3d<value_type> Vec3d<value_type>::operator/(const value_type& x) const
+{
+	return Vec3d<value_type>(this->v[0] / x, this->v[1] / x, this->v[2] / x);
+}
+
+template<class value_type>
 value_type dot(const Vec3d<value_type>& v1, const Vec3d<value_type>& v2)
 {
 	return v1.v[0] * v2.v[0] + v1.v[1] * v2.v[1] + v1.v[2] * v2.v[2];
@@ -38,6 +55,20 @@ template<class value_type>
 Vec3d<value_type> cross(const Vec3d<value_type>& v1, const Vec3d<value_type>& v2)
 {
 	return Vec3d<value_type>(v1.v[1] * v2.v[2] - v1.v[2] * v2.v[1], v1.v[2] * v2.v[0] - v1.v[0] * v2.v[2], v1.v[0] * v2.v[1] - v1.v[1] * v2.v[0]);
+}
+
+template<class value_type>
+value_type Vec3d<value_type>::length()
+{
+	return std::sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+}
+
+template<class value_type>
+value_type Vec3d<value_type>::normalize()
+{
+	value_type len = this->length();
+	this->operator/(len);
+	return len;
 }
 
 typedef Vec3d<float> flt3;
