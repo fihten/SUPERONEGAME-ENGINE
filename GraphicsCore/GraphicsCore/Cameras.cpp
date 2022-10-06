1fa2cbf0199fc65b20617e73d386921d2e865565
Andrey Maly <fihten1991@gmail.com>
2022-10-06 23:27:31 +0300

    void loadCamerasCFGs()

--- GraphicsCore/GraphicsCore/Cameras.cpp
+++ GraphicsCore/GraphicsCore/Cameras.cpp
@@ -1,2 +1,21 @@
 #include "Cameras.h"
+#include <tchar.h>
+#include <string>
 
+Camera cameras[CAMERAS_NUMBER];
+
+void loadCamerasCFGs()
+{
+	char cfgsFolder[200];
+	int sz = sizeof cfgsFolder / sizeof * cfgsFolder;
+	GetEnvironmentVariableA("CONFIGS", cfgsFolder, sz);
+	
+	std::string sCameraCFGtemplate = std::string(cfgsFolder) + "\\Camera%d.cfg";
+	for (int i = 0; i < CAMERAS_NUMBER; ++i)
+	{
+		char szCameraCFG[200];
+		sprintf(szCameraCFG, sCameraCFGtemplate.c_str(), i);
+
+		cameras[i].loadCFG(szCameraCFG);
+	}
+}
--- GraphicsCore/GraphicsCore/Cameras.h
+++ GraphicsCore/GraphicsCore/Cameras.h
@@ -1,1 +1,7 @@
 #pragma once
+#include "Camera.h"
+
+#define CAMERAS_NUMBER 16
+extern Camera cameras[CAMERAS_NUMBER];
+
+void loadCamerasCFGs();
