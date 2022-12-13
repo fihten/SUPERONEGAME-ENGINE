5c72d96e44e8c3fcdb16c17c0c00cc0eb7b4fa50
Andrey Maly <fihten1991@gmail.com>
2022-12-13 21:53:45 +0300

    fetchLine

--- GraphicsCore/GraphicsCore/preprocessor.cpp
+++ GraphicsCore/GraphicsCore/preprocessor.cpp
@@ -1,9 +1,24 @@
 #include "preprocessor.h"
 
+bool fetchLine(const std::string& code, int start, int& end, std::string& line);
+
 std::string preprocess(
 	const std::string& code,
 	std::map<std::string, std::string>& defines
 )
+{
+	std::string res = "";
+	std::string line = "";
+	int start = 0;
+	int end = 0;
+	// lines fetching
+	while (fetchLine(res, start, end, line))
+	{
+
+	}
+}
+
+bool fetchLine(const std::string& code, int start, int& end, std::string& line)
 {
 
 }
