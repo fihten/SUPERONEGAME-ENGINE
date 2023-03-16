5f212f9a3be0fb4e607e03100bc173cf42b67ae4
Andrey Maly <fihten1991@gmail.com>
2023-03-16 21:35:41 +0300

    Timer

---
+++ Timer/Timer/Timer.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "Timer.h"
+
+Timer* Timer::timer = 0;
+float Timer::time = 0;
+
+Timer::Timer()
+{
+	__int64 count = 0;
+	QueryPerformanceCounter((LARGE_INTEGER*)& count);
+
+	__int64 countsPerSec = 0;
+	QueryPerformanceFrequency((LARGE_INTEGER*)& countsPerSec);
+
+	double secsPerCount = 1.0 / (double)countsPerSec;
+
+	time = count * secsPerCount;
+}
+
+Timer::~Timer()
+{
+	timer = 0;
+	time = 0;
+}
+
+Timer* Timer::Instance()
+{
+	if (timer == 0)
+		timer = new Timer();
+	return timer;
+}
+
+float Timer::elapsedTime()
+{
+	__int64 count = 0;
+	QueryPerformanceCounter((LARGE_INTEGER*)& count);
+
+	__int64 countsPerSec = 0;
+	QueryPerformanceFrequency((LARGE_INTEGER*)& countsPerSec);
+
+	float newTime = (double)count / (double)countsPerSec;
+
+	float result = newTime > time ? newTime - time : 0.0f;
+
+	time = newTime;
+
+	return result;
+}
---
+++ Timer/Timer/Timer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <windows.h>
+
+class Timer
+{
+	Timer();
+public:
+	~Timer();
+	static Timer* Instance();
+	float elapsedTime();
+private:
+	static Timer* timer;
+	static float time;
+};
+
