cb41c56a2a4a1b94cf6197a62bdec45b0e4ae27d
Andrey Maly <fihten1991@gmail.com>
2023-03-27 18:32:29 +0300

    StringToNumbers

---
+++ MathCore/MathCore/StringToNumbers.cpp
@@ -0,0 +1,34 @@
+#include "StringToNumbers.h"
+
+void stringToNumbers(const std::string& str, float numbers[], int maxCount)
+{
+	int pos = str.find_first_of("{");
+	if (pos == str.npos)
+		return;
+
+	pos += 1;
+	
+	int index = 0;
+	pos = str.find_first_not_of(" ", pos);
+	while (pos != str.npos && str[pos] != '}' && index < maxCount)
+	{
+		int startOfNumber = str.find_first_not_of(" ", pos);
+		if (startOfNumber == str.npos)
+			return;
+
+		int endOfNumber = str.find_first_of(" ,}", startOfNumber);
+		if (endOfNumber == str.npos)
+			return;
+
+		std::string numberStr = str.substr(startOfNumber, endOfNumber - startOfNumber);
+		numbers[index++] = std::atof(numberStr.c_str());
+
+		pos = endOfNumber;
+		if(str[pos] == ' ')
+			pos = str.find_first_of(",}", pos);
+		if (str[pos] == ',')
+			++pos;
+
+		pos = str.find_first_not_of(" ", pos);
+	}
+}
---
+++ MathCore/MathCore/StringToNumbers.h
@@ -0,0 +1,4 @@
+#pragma once
+#include <string>
+
+void stringToNumbers(const std::string& str, float numbers[], int maxCount = INT_MAX);
