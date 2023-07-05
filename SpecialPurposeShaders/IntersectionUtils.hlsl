6bda2a8a51d2d7b3c1daaae2bd1a6c8e0750a5b1
Andrey Maly <fihten1991@gmail.com>
2023-07-05 20:53:34 +0300

    checkIntersection()

--- SpecialPurposeShaders/IntersectionUtils.hlsl
+++ SpecialPurposeShaders/IntersectionUtils.hlsl
@@ -6,6 +6,8 @@
 
 uint findIntersection(float4 plane, Segment seg, out float t)
 {
+	t = 0;
+
 	float N = dot(plane.xyz, seg.v0) + plane.w;
 	float D = dot(plane.xyz, seg.v1 - seg.v0);
 
@@ -47,10 +49,45 @@ bool checkIntersection(Envelope env, Triangle tri, float threshold)
 	r0[RIGHT_PLANE]  =  float3(env.max.x, 0, 0);
 
 	float3 min[PLANE_NUMBER];
-	min[FRONT_PLANE] = float3(env.min.xy - threshold, -threshold);
-	min[BACK_PLANE] = float3(env.min.xy - threshold, 1.0f - threshold);
+	min[FRONT_PLANE]  = float3(env.min.xy - threshold, -threshold);
+	min[BACK_PLANE]   = float3(env.min.xy - threshold, 1.0f - threshold);
 	min[BOTTOM_PLANE] = float3(env.min.xy - threshold, -threshold);
-	min[TOP_PLANE] = float3(env.min.x - threshold, env.max.y - threshold, -threshold);
+	min[TOP_PLANE]    = float3(env.min.x - threshold, env.max.y - threshold, -threshold);
+	min[LEFT_PLANE]   = float3(env.min.xy - threshold, -threshold);
+	min[RIGHT_PLANE]  = float3(env.max.x - threshold, end.min.y - threshold, -threshold);
+
+	float3 max[PLANE_NUMBER];
+	max[FRONT_PLANE]  = float3(env.max.xy + threshold, threshold);
+	max[BACK_PLANE]   = float3(env.max.xy + threshold, 1.0f + threshold);
+	max[BOTTOM_PLANE] = float3(env.max.x + threshold, env.min.y + threshold, 1.0f + threshold);
+	max[TOP_PLANE]    = float3(env.max.xy + threshold, 1.0f + threshold);
+	max[LEFT_PLANE]   = float3(env.min.x + threshold, env.max.y + threshold, 1.0f + threshold);
+	max[RIGHT_PLANE]  = float3(env.max.xy + threshold, 1.0f + threshold);
+
+	Segment segments[3];
+	
+	segments[0].v0 = tri.v0;
+	segments[0].v1 = tri.v1;
+
+	segments[1].v0 = tri.v1;
+	segments[1].v1 = tri.v2;
+
+	segments[2].v0 = tri.v0;
+	segments[2].v1 = tri.v2;
+
+	[unroll]
+	for (int pi = 0; pi < PLANE_NUMBER; ++pi)
+	{
+		float4 plane = float4(n[pi], -dot(n[pi], r0[pi]));
+		for (int si = 0; si < 3; ++si)
+		{
+			float t = 0;
+			if (findIntersection(plane, segments[si], t) == 0)
+				continue;
+
+
+		}
+	}
 }
 
 #endif
