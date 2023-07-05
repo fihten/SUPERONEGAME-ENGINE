22457982fb18008778d6717b12d0eebaa4c33514
Andrey Maly <fihten1991@gmail.com>
2023-07-05 06:53:35 +0300

    checkIntersection()

--- SpecialPurposeShaders/IntersectionUtils.hlsl
+++ SpecialPurposeShaders/IntersectionUtils.hlsl
@@ -19,9 +19,38 @@ uint findIntersection(float4 plane, Segment seg, out float t)
 	return 1;
 }
 
-bool checkIntersection(Envelope env, Triangle tri)
-{
+#define FRONT_PLANE    0
+#define BACK_PLANE     1
+#define BOTTOM_PLANE   2
+#define TOP_PLANE      3
+#define LEFT_PLANE     4
+#define RIGHT_PLANE    5
 
+#define PLANE_NUMBER   6
+
+bool checkIntersection(Envelope env, Triangle tri, float threshold)
+{
+	float3 n[PLANE_NUMBER];
+	n[FRONT_PLANE]  = float3( 0,  0, -1);
+	n[BACK_PLANE]   = float3( 0,  0, +1);
+	n[BOTTOM_PLANE] = float3( 0, -1,  0);
+	n[TOP_PLANE]    = float3( 0, +1,  0);
+	n[LEFT_PLANE]   = float3(-1,  0,  0);
+	n[RIGHT_PLANE]  = float3(+1,  0,  0);
+
+	float3 r0[PLANE_NUMBER];
+	r0[FRONT_PLANE]  =  float3(0, 0, 0);
+	r0[BACK_PLANE]   =  float3(0, 0, 1);
+	r0[BOTTOM_PLANE] =  float3(0, env.min.y, 0);
+	r0[TOP_PLANE]    =  float3(0, env.max.y, 0);
+	r0[LEFT_PLANE]   =  float3(env.min.x, 0, 0);
+	r0[RIGHT_PLANE]  =  float3(env.max.x, 0, 0);
+
+	float3 min[PLANE_NUMBER];
+	min[FRONT_PLANE] = float3(env.min.xy - threshold, -threshold);
+	min[BACK_PLANE] = float3(env.min.xy - threshold, 1.0f - threshold);
+	min[BOTTOM_PLANE] = float3(env.min.xy - threshold, -threshold);
+	min[TOP_PLANE] = float3(env.min.x - threshold, env.max.y - threshold, -threshold);
 }
 
 #endif
